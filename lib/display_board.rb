74cab8fa7ccada4b1ec93024ed9fd23cfdf61ded
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2019-08-07 14:11:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,9 @@
-board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{board[0]} " "|"" #{board[1]} ""|"" #{board[2]} ""\n-----------\n"" #{board[3]} ""|"" #{board[4]} ""|"" #{board[5]} ""\n-----------\n"" #{board[6]} ""|"" #{board[7]} ""|"" #{board[8]} "
-  end
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
